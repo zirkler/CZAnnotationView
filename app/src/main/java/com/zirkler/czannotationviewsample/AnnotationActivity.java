5c601e64e4c5818d1a1c20303aafabb7238c9a7a
zirkler <zirkler@users.noreply.github.com>
2016-11-30 14:05:17 +0100

    improve example application

---
+++ app/src/main/java/com/zirkler/czannotationviewsample/AnnotationActivity.java
@@ -0,0 +1,156 @@
+package com.zirkler.czannotationviewsample;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+import com.zirkler.czannotationviewsample.AnnotationView.CZAttacher;
+import com.zirkler.czannotationviewsample.AnnotationView.CZDrawingActionEraser;
+import com.zirkler.czannotationviewsample.AnnotationView.CZDrawingActionFreehand;
+import com.zirkler.czannotationviewsample.AnnotationView.CZIDrawingAction;
+import com.zirkler.czannotationviewsample.AnnotationView.CZIItemLongClickListener;
+import com.zirkler.czannotationviewsample.AnnotationView.CZPhotoView;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AnnotationActivity extends AppCompatActivity {
+
+    public static final String DRAWN_ACTIONS = "drawn_actions";
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    private CZAttacher mAttacher;
+    private CZPhotoView mPhotoView;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.annotation_activity);
+        ButterKnife.bind(this);
+        setupToolbar();
+
+        mPhotoView = (CZPhotoView) findViewById(R.id.iv_photo);
+        Picasso.with(this).load(R.drawable.ffuf_office).into(mPhotoView, new Callback() {
+            @Override
+            public void onSuccess() {
+                // The MAGIC happens here!
+                mAttacher = new CZAttacher(mPhotoView);
+
+                // Set default drawing tool
+                mPhotoView.setCurrentDrawingAction(new CZDrawingActionFreehand(AnnotationActivity.this, null));
+            }
+
+            @Override
+            public void onError() {
+                Log.e(AnnotationActivity.class.getSimpleName(), "Picasso Error occurred.");
+            }
+        });
+
+        mPhotoView.setOnItemLongClickListener(new CZIItemLongClickListener() {
+            @Override
+            public void onItemLongClicked(CZIDrawingAction item, MotionEvent e) {
+                Toast.makeText(AnnotationActivity.this, "Item got clicked", Toast.LENGTH_SHORT).show();
+                item.moveStart();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.annotation_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else if (item.getItemId() == R.id.action_freehand_drawing) {
+            mPhotoView.setCurrentDrawingAction(new CZDrawingActionFreehand(this, null));
+        } else if (item.getItemId() == R.id.action_eraser) {
+            mPhotoView.setCurrentDrawingAction(new CZDrawingActionEraser(this, null));
+        } else if (item.getItemId() == R.id.action_pick_background) {
+
+        } else if (item.getItemId() == R.id.action_measurement_line) {
+
+        } else if (item.getItemId() == R.id.action_serialize) {
+            FileOutputStream fos;
+            try {
+                String fileName = "serialized.txt";
+
+                // write file
+                fos = this.openFileOutput(fileName, Context.MODE_PRIVATE);
+                ObjectOutputStream os = new ObjectOutputStream(fos);
+                os.writeObject(mPhotoView.getDrawnActions());
+                os.close();
+                fos.close();
+
+                // read file
+                FileInputStream fis = this.openFileInput(fileName);
+                ObjectInputStream is = new ObjectInputStream(fis);
+                List<CZIDrawingAction> simpleClass = (List<CZIDrawingAction>) is.readObject();
+                is.close();
+                fis.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+
+                Log.i("asd", sw.toString());
+                Toast.makeText(this, sw.toString(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+        return true;
+    }
+
+    @OnClick(R.id.bttUndo)
+    public void bttUndoClicked() {
+        mPhotoView.undo();
+    }
+
+    @OnClick(R.id.bttRedo)
+    public void bttRedoClicked() {
+        mPhotoView.redo();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAttacher.cleanup();
+    }
+
+    private void setupToolbar() {
+        setTitle("Annotation View");
+        mToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Drawable upArrow = ResourcesCompat.getDrawable(getResources(), R.drawable.abc_ic_ab_back_material, null);
+        upArrow.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+    }
+}
--- app/src/main/java/com/zirkler/czannotationviewsample/MainActivity.java
+++ app/src/main/java/com/zirkler/czannotationviewsample/MainActivity.java
@@ -1,136 +1,108 @@
 package com.zirkler.czannotationviewsample;
 
-import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
-import com.zirkler.czannotationviewsample.AnnotationView.CZAttacher;
-import com.zirkler.czannotationviewsample.AnnotationView.CZDrawingActionEraser;
-import com.zirkler.czannotationviewsample.AnnotationView.CZDrawingActionFreehand;
-import com.zirkler.czannotationviewsample.AnnotationView.CZIDrawingAction;
-import com.zirkler.czannotationviewsample.AnnotationView.CZIItemLongClickListener;
-import com.zirkler.czannotationviewsample.AnnotationView.CZPhotoView;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.List;
-
+import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String DRAWN_ACTIONS = "drawn_actions";
-    private CZAttacher mAttacher;
-    private CZPhotoView mPhotoView;
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.recyclerview) RecyclerView mRecyclerView;
+    @BindView(R.id.appbarLayout) AppBarLayout mAppbarLayout;
+    private DrawingsAdapter mRecyclerAdapter;
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
         ButterKnife.bind(this);
-
-        mPhotoView = (CZPhotoView) findViewById(R.id.iv_photo);
-        Picasso.with(this).load(R.drawable.ffuf_office).into(mPhotoView, new Callback() {
-            @Override
-            public void onSuccess() {
-                // The MAGIC happens here!
-                mAttacher = new CZAttacher(mPhotoView);
-
-                // set default paint tool
-                mPhotoView.setCurrentDrawingAction(new CZDrawingActionFreehand(MainActivity.this, null));
-            }
-
-            @Override
-            public void onError() {
-                Log.e(MainActivity.class.getSimpleName(), "Picasso Error occurred.");
-            }
-        });
-
-        mPhotoView.setOnItemLongClickListener(new CZIItemLongClickListener() {
-            @Override
-            public void onItemLongClicked(CZIDrawingAction item, MotionEvent e) {
-                Toast.makeText(MainActivity.this, "Item got clicked", Toast.LENGTH_SHORT).show();
-                item.moveStart();
-            }
-        });
+        setupToolbar();
+        setupRecyclerView();
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_freehand_drawing) {
-            mPhotoView.setCurrentDrawingAction(new CZDrawingActionFreehand(this, null));
-        } else if (item.getItemId() == R.id.action_eraser) {
-            mPhotoView.setCurrentDrawingAction(new CZDrawingActionEraser(this, null));
-        } else if (item.getItemId() == R.id.action_pick_background) {
-
-        } else if (item.getItemId() == R.id.action_measurement_line) {
-
-        } else if (item.getItemId() == R.id.action_serialize) {
-            FileOutputStream fos;
-            try {
-                String fileName = "serialized.txt";
-
-                // write file
-                fos = this.openFileOutput(fileName, Context.MODE_PRIVATE);
-                ObjectOutputStream os = new ObjectOutputStream(fos);
-                os.writeObject(mPhotoView.getDrawnActions());
-                os.close();
-                fos.close();
-
-                // read file
-                FileInputStream fis = this.openFileInput(fileName);
-                ObjectInputStream is = new ObjectInputStream(fis);
-                List<CZIDrawingAction> simpleClass = (List<CZIDrawingAction>) is.readObject();
-                is.close();
-                fis.close();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                StringWriter sw = new StringWriter();
-                PrintWriter pw = new PrintWriter(sw);
-                e.printStackTrace(pw);
-
-                Log.i("asd", sw.toString());
-                Toast.makeText(this, sw.toString(), Toast.LENGTH_LONG).show();
-            }
-
+        if (item.getItemId() == R.id.action_add) {
+            Intent intent = new Intent(this, AnnotationActivity.class);
+            startActivity(intent);
         }
         return true;
     }
 
-    @OnClick(R.id.bttUndo)
-    public void bttUndoClicked() {
-        mPhotoView.undo();
+    private void setupToolbar() {
+        setTitle("CZAnnotationView Example Usage");
+        mToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(mToolbar);
     }
 
-    @OnClick(R.id.bttRedo)
-    public void bttRedoClicked() {
-        mPhotoView.redo();
+    private void setupRecyclerView() {
+        mRecyclerAdapter = new DrawingsAdapter();
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public class DrawingItemHolder extends RecyclerView.ViewHolder {
+
+        View mItemView;
+        @BindView(R.id.txtIndex) TextView mTxtIndex;
+
+        public DrawingItemHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mItemView = itemView;
+        }
+
+        public void bind(int index) {
+            mTxtIndex.setText("Drawing " + String.valueOf(index));
+
+            mItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, AnnotationActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    public class DrawingsAdapter extends RecyclerView.Adapter<DrawingItemHolder> {
+
+        @Override
+        public DrawingItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.list_item, parent, false);
+            return new DrawingItemHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(DrawingItemHolder holder, int position) {
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
     }
 
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mAttacher.cleanup();
-    }
 }
